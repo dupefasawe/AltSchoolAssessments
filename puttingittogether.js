b8ef72d2ad2561e59a41d65c79923cefbc51a4fe
Dupe Fasawe <64515839+dupefasawe@users.noreply.github.com>
2025-12-20 15:55:31 -0600

    realized it's linked to the wrong recipe page

---
+++ puttingittogether.js
@@ -0,0 +1,7 @@
+
+let usernames = ["unknown", "hi", "my", "name"]
+for(let username of usernames){    console.log(username);
+}
+//while (username.length > 3) i++ 
+//alert(username[2]);
+//console.log("username length is " + username.length)
